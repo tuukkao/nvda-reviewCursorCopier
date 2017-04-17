42141190b95fdc341893fe848a802555bf829e48
Tuukka Ojala <tuukka.ojala@gmail.com>
2017-04-17 10:57:24 +0300

    Set script category globally instead of per script.

--- addon/globalPlugins/reviewCursorCopier/__init__.py
+++ addon/globalPlugins/reviewCursorCopier/__init__.py
@@ -28,38 +28,34 @@ def copyReviewUnitToClipboard(unit, copyFrom=None):
         ui.message(_("Failed to copy"))
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+    scriptCategory = globalCommands.SCRCAT_TEXTREVIEW
+
     def script_copyLineToClipboard(self, gesture):
         copyReviewUnitToClipboard(textInfos.UNIT_LINE)
     # Translators: Describes the "copy line to clipboard" command.
     script_copyLineToClipboard.__doc__ = "Copies the line under the review cursor to the clipboard."
-    script_copyLineToClipboard.category = globalCommands.SCRCAT_TEXTREVIEW
 
     def script_copyWordToClipboard(self, gesture):
         copyReviewUnitToClipboard(textInfos.UNIT_WORD)
     # Translators: Describes the "copy word to clipboard" command.
     script_copyWordToClipboard.__doc__ = "Copies the word under the review cursor to the clipboard."
-    script_copyWordToClipboard.category = globalCommands.SCRCAT_TEXTREVIEW
 
     def script_copyPrecedingLineToClipboard(self, gesture):
         copyReviewUnitToClipboard(textInfos.UNIT_LINE, copyFrom="endToStart")
     # Translators: Describes the "copy preceding line to clipboard" command.
     script_copyPrecedingLineToClipboard.__doc__ = "Copies the text from the start of the current line to the clipboard."
-    script_copyPrecedingLineToClipboard.category = globalCommands.SCRCAT_TEXTREVIEW
 
     def script_copyRemainingLineToClipboard(self, gesture):
         copyReviewUnitToClipboard(textInfos.UNIT_LINE, copyFrom="startToStart")
     # Translators: Describes the "copy remaining line to clipboard" command.
     script_copyRemainingLineToClipboard.__doc__ = "Copies the text until the end of the current line to the clipboard."
-    script_copyRemainingLineToClipboard.category = globalCommands.SCRCAT_TEXTREVIEW
 
     def script_copyPrecedingWordToClipboard(self, gesture):
         copyReviewUnitToClipboard(textInfos.UNIT_WORD, copyFrom="endToStart")
     # Translators: Describes the "copy preceding word to clipboard" command.
     script_copyPrecedingWordToClipboard.__doc__ = "Copies the text from the start of the current word to the clipboard."
-    script_copyPrecedingWordToClipboard.category = globalCommands.SCRCAT_TEXTREVIEW
 
     def script_copyRemainingWordToClipboard(self, gesture):
         copyReviewUnitToClipboard(textInfos.UNIT_WORD, copyFrom="startToStart")
     # Translators: Describes the "copy remaining word to clipboard" command.
     script_copyRemainingWordToClipboard.__doc__ = "Copies the text until the end of the current word to the clipboard."
-    script_copyRemainingWordToClipboard.category = globalCommands.SCRCAT_TEXTREVIEW
