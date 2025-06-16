2de418bc4c0e45e2844f288dba964ea5a05e5b9a
Tuukka Ojala <tuukka.ojala@iki.fi>
2025-06-16 16:33:37 +0300

    Bump version to 1.8 and api compatibility to 2025.1

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""Provides various commands for copying the text under the review cursor to the clipboard. Please consult the documentation for more information."""),
 	# version
-	"addon_version": "1.7",
+	"addon_version": "1.8",
 	# Author(s)
 	"addon_author": "Tuukka Ojala <tuukka.ojala@iki.fi>",
 	# URL for the add-on documentation support
@@ -37,7 +37,7 @@ addon_info = {
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion": "0.0.0",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2024.1",
+	"addon_lastTestedNVDAVersion": "2025.1",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
